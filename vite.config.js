af040d771e890d9c74b6eb14e0b80a6c7b4473ef
wayme <rm.ivaniuk@gmail.com>
2023-11-26 10:04:43 +0200

    add deployment files

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-podcast/',
   server: {
     open: true,
   },
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     sourcemap: true,
   },
 });
