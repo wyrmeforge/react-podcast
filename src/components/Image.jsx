ba77b10f5c9e5d0443aed7ef1cc5aac178a3f9a6
wayme <rm.ivaniuk@gmail.com>
2023-12-09 18:47:24 +0200

    fix images

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-const baseUrl = 'http://localhost:1337';
-
 const Image = ({ src, alt, className }) => {
-  const url = src.startsWith('/src') ? src : `${baseUrl}${src}`;
-
-  return <img className={className} src={url} alt={alt} />;
+  return <img className={className} src={src} alt={alt} />;
 };
 
 export default Image;
